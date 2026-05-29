1e44e8c959380e99aae5a0a07a85cd89f4ce30e6
Cristiano Lima Ribeiro <cristiano.lima.ribeiro@gmail.com>
2026-05-29 11:59:07 -0300

    feat: add animated mesh gradient background

--- portfolio-new/src/app/layout.tsx
+++ portfolio-new/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MeshGradient } from "@/components/MeshGradient";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <MeshGradient />
+        {children}
+      </body>
     </html>
   );
 }
